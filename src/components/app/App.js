0b8629416d66ce3857c8ff99cc27bbb0fb9e7a86
Korsaurus <grkorsos@gmail.com>
2021-10-21 12:41:47 -0600

    Finished

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,20 +1,26 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from '../chatFeed/chatFeed';
+import LoginForm from '../login/login';
 import './App.css';
-import { ChatEngine } from "react-chat-engine"
-import ChatFeed from "../chatFeed/chatFeed"
+
+const projectID = '2ca59af7-10ac-4497-80a2-55d4f0d5c5f2';
 
 const App = () => {
-    return (
-        <div className="App">
-            <ChatEngine
-                height="100vh"
-                projectID="
-                2ca59af7-10ac-4497-80a2-55d4f0d5c5f2"
-                userName="Korsaurus"
-                userSecret="123123"
-                renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps} />}
-            />
-        </div>
-    );
-}
+  if (!localStorage.getItem('username')) return <LoginForm />
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
--- src/components/messageForm/myMessage.jsx
+++ src/components/messageForm/myMessage.jsx
@@ -1,20 +1,20 @@
 const MyMessage = ({ message }) => {
-  // message defines the prop that will be passed to the message form component
-  // Checks if the message is an image
-  if (message?.attachments?.length > 0) {
+    if (message.attachments && message.attachments.length > 0) {
+      return (
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ float: 'right' }}
+        />
+      );
+    }
+  
     return (
-      <img
-        src={message.attachments[0].file}
-        alt="message-attachment"
-        className="message-image"
-        style={{ float: "right" }}
-      />
+      <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+        {message.text}
+      </div>
     );
-  }
-
-  return <div className="message" style={{ float: "right", marginRight: "18px", color:"white", backgroundColor: "#3B2A50" }}>
-      {message.text}
-  </div>;
-};
-
-export default MyMessage;
+  };
+  
+  export default MyMessage;
